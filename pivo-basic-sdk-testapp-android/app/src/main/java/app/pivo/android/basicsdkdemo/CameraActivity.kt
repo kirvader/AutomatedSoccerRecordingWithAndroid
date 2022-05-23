6861a4b9bbc739d61a01e2d807fde9e9dfb0cd66
kirvader <kirkondratiuk@gmail.com>
2022-05-24 01:24:51 +0300

    pivo upd

--- pivo-basic-sdk-testapp-android/app/src/main/java/app/pivo/android/basicsdkdemo/CameraActivity.kt
+++ pivo-basic-sdk-testapp-android/app/src/main/java/app/pivo/android/basicsdkdemo/CameraActivity.kt
@@ -3,32 +3,52 @@ package app.pivo.android.basicsdkdemo
 import ai.onnxruntime.OrtEnvironment
 import ai.onnxruntime.OrtSession
 import android.Manifest
+import android.content.ContentValues
 import android.content.pm.PackageManager
+import android.icu.text.SimpleDateFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
-import androidx.camera.core.*
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.util.Consumer
 import app.pivo.android.basicsdk.PivoSdk
 import kotlinx.android.synthetic.main.activity_camera.*
 import kotlinx.coroutines.*
 import java.lang.Runnable
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import kotlin.math.abs
 import kotlin.math.pow
 
 class CameraActivity : AppCompatActivity() {
+
+    private var mediaStoreOutputOptions: MediaStoreOutputOptions? = null
+    private var recorder: Recorder? = null
+    private var selector: QualitySelector? = null
+    private val cameraExecutor: ExecutorService by lazy { Executors.newSingleThreadExecutor() }
+
     private val backgroundExecutor: ExecutorService by lazy { Executors.newSingleThreadExecutor() }
     private val labelData: List<String> by lazy { readLabels() }
-    private val scope = CoroutineScope(Job() + Dispatchers.Main)
+    private val scope = CoroutineScope(Job() + Dispatchers.Default)
 
-    private lateinit var ortEnv: OrtEnvironment
-    private lateinit var imageCapture: ImageCapture
-    private lateinit var imageAnalysis: ImageAnalysis
+    private var ortEnv: OrtEnvironment? = null
+    private var imageCapture: ImageCapture? = null
+    private var imageAnalysis: ImageAnalysis? = null
+    private var videoCapture: VideoCapture<Recorder>? = null
+
+    private var recording: Recording? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,21 +62,106 @@ class CameraActivity : AppCompatActivity() {
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
             )
         }
+
+        videoCaptureButton.setOnClickListener {
+            toggleCameraRecording()
+        }
     }
+
+    private val recordingListener = Consumer<VideoRecordEvent> { event ->
+        when(event) {
+            is VideoRecordEvent.Start -> {
+                val msg = "Capture Started"
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT)
+                    .show()
+                // update app internal recording state
+
+            }
+            is VideoRecordEvent.Finalize -> {
+                val msg = if (!event.hasError()) {
+                    "Video capture succeeded: ${event.outputResults.outputUri}"
+                    // TODO() handle succeeding here
+                } else {
+                    // update app state when the capture failed.
+                    recording?.close()
+                    recording = null
+
+                    "Video capture ends with error: ${event.error}"
+                }
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+    private fun toggleCameraRecording() {
+        if (recording != null) {
+            stopCapturing()
+        } else {
+            startCapture()
+        }
+    }
+
+    private fun startCapture() {
+        val name = "CameraX-recording-" +
+                SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+                    .format(System.currentTimeMillis()) + ".mp4"
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Video.Media.DISPLAY_NAME, name)
+        }
+
+        mediaStoreOutputOptions = MediaStoreOutputOptions
+            .Builder(this.contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+            .setContentValues(contentValues)
+            .build()
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        recording = videoCapture?.output
+            ?.prepareRecording(this, mediaStoreOutputOptions!!)
+            ?.withAudioEnabled()
+            ?.start(ContextCompat.getMainExecutor(this), recordingListener)!!
+    }
+
+    private fun stopCapturing() {
+        recording?.stop()
+        recording = null
+    }
+
+
 
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
         cameraProviderFuture.addListener(Runnable {
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            selector = QualitySelector
+                .from(
+                    Quality.SD,
+                    FallbackStrategy.higherQualityOrLowerThan(Quality.SD)
+                )
+
+            recorder = Recorder.Builder()
+                .setExecutor(cameraExecutor).setQualitySelector(selector!!)
+                .build()
+
+            videoCapture = VideoCapture.withOutput(recorder!!)
 
             // Preview
             val preview = Preview.Builder()
 //                .setTargetAspectRatio(AspectRatio.RATIO_16_9)
                 .build()
-                .also {
-                    it.setSurfaceProvider(viewFinder.surfaceProvider)
-                }
 
             imageCapture = ImageCapture.Builder()
 //                .setTargetAspectRatio(AspectRatio.RATIO_16_9)
@@ -70,15 +175,15 @@ class CameraActivity : AppCompatActivity() {
 
             setORTAnalyzer()
 
-            try {
-                cameraProvider.unbindAll()
 
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture, imageAnalysis
-                )
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
+            cameraProvider.unbindAll()
+
+            cameraProvider.bindToLifecycle(
+                this, cameraSelector, preview, videoCapture, imageAnalysis
+            )
+
+            preview.setSurfaceProvider(viewFinder.surfaceProvider)
+
 
 
         }, ContextCompat.getMainExecutor(this))
@@ -92,6 +197,8 @@ class CameraActivity : AppCompatActivity() {
         super.onDestroy()
         backgroundExecutor.shutdown()
         ortEnv?.close()
+        ProcessCameraProvider.getInstance(this).get().unbindAll()
+
     }
 
     override fun onRequestPermissionsResult(
@@ -127,9 +234,7 @@ class CameraActivity : AppCompatActivity() {
     }
 
     // translates x to [-1; 1]
-    private fun normalizeBoxCoord(x: Float): Float {
-        return x * 2.0F - 1.0F
-    }
+    private fun normalizeBoxCoord(x: Float): Float = x * 2.0F - 1.0F
 
     private fun handlePivoPod(box_x: Float) {
         // normalizing
@@ -138,15 +243,19 @@ class CameraActivity : AppCompatActivity() {
         var angle = 0
         var speed = 0
 
-        if (abs_val >= 0.6F) {
-            angle = 10
-            speed = 10
-        } else if (abs_val >= 0.2F) {
-            angle = 5
-            speed = 20
-        } else {
-            PivoSdk.getInstance().stop()
-            return
+        when {
+            abs_val >= 0.6F -> {
+                angle = 10
+                speed = 10
+            }
+            abs_val >= 0.2F -> {
+                angle = 5
+                speed = 20
+            }
+            else -> {
+                PivoSdk.getInstance().stop()
+                return
+            }
         }
         if (x < 0) {
             PivoSdk.getInstance().turnLeft(angle, speed)
@@ -186,9 +295,8 @@ class CameraActivity : AppCompatActivity() {
     }
 
     // Read MobileNet V2 classification labels
-    private fun readLabels(): List<String> {
-        return resources.openRawResource(R.raw.model_classes).bufferedReader().readLines()
-    }
+    private fun readLabels(): List<String> =
+        resources.openRawResource(R.raw.model_classes).bufferedReader().readLines()
 
     // Read ort model into a ByteArray, run in background
     private suspend fun readModel(): ByteArray = withContext(Dispatchers.IO) {
@@ -217,5 +325,6 @@ class CameraActivity : AppCompatActivity() {
         public const val TAG = "ORTImageClassifier"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
     }
 }
